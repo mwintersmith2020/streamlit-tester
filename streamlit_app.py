8d6d87d39c868e8c997b6a3e54f943b4947debf4
mwintersmith2020 <54284704+mwintersmith2020@users.noreply.github.com>
2024-10-23 18:36:15 -0700

    Update streamlit_app.py
    Add sqlite3 version check...

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+import sys
+st.write(f"Python version: {sys.version}")
+
+import sqlite3
+st.write(f"SQLite version: {sqlite3.sqlite_version}")
+
+
